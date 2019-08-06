98bbd1c42d81f752c4d8500e115971bf06f28345
ansamots <33481586+ansamots@users.noreply.github.com>
2019-08-06 07:17:21 +0300

    Added Controller

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,17 @@
 package sample;
 
+import javafx.event.ActionEvent;
+
 public class Controller {
+    public void logInButton(ActionEvent actionEvent){
+        System.out.println("Press button LogIn");
+    }
+
+    public void settingsButton(ActionEvent actionEvent) {
+        System.out.println("Press button Settings");
+    }
+
+    public void registeredButton(ActionEvent actionEvent) {
+        System.out.println("Press button Regidtered");
+    }
 }
