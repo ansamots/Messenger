ba567edc91971ecad5dba442eed6b432ed484134
ansamots <33481586+ansamots@users.noreply.github.com>
2019-08-09 17:36:07 +0300

    Added Controller end Fields

--- src/sample/controllers/Controller.java
+++ src/sample/controllers/Controller.java
@@ -1,10 +1,24 @@
-package sample;
+package sample.controllers;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 
 public class Controller {
+    @FXML
+    private TextField logonField;
+
+    @FXML
+    private TextField passwordField;
+
     public void logInButton(ActionEvent actionEvent){
+
         System.out.println("Press button LogIn");
+        String login = logonField.getText();
+        String password = passwordField.getText();
+        System.out.println(logonField + " "+ passwordField);
+        System.out.println(login + " " + password);
     }
 
     public void settingsButton(ActionEvent actionEvent) {
@@ -14,4 +28,5 @@ public class Controller {
     public void registeredButton(ActionEvent actionEvent) {
         System.out.println("Press button Regidtered");
     }
+
 }
