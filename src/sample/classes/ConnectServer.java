75bc5d0e430779e537e3cc25961286213252106e
ansamots <33481586+ansamots@users.noreply.github.com>
2020-01-18 09:03:56 +0300

    Доработка классов.

--- src/sample/classes/ConnectServer.java
+++ src/sample/classes/ConnectServer.java
@@ -1,6 +1,24 @@
 package sample.classes;
 
 public class ConnectServer {
+    /**
+     * Контроллер для проверки проверки доступности сервера
+     * @param ip
+     * @param port
+     */
+    ConnectServer(String ip, String port){
+        this.ip = ip;
+        this.port = port;
+        checkingAvailabilityServer();
+    }
+
+    /**
+     * Контроллер используется для авторизации в системе.
+     * @param ip
+     * @param port
+     * @param login
+     * @param password
+     */
     ConnectServer(String ip, String port, String login, String password){
         this.ip = ip;
         this.port = port;
@@ -9,6 +27,9 @@ public class ConnectServer {
         startLogin();
     }
 
+    /**
+     * Метод для авторизации в системе по указанным параметрам
+     */
     private void startLogin(){
         checkingAvailabilityServer();
         if(result){
@@ -18,10 +39,18 @@ public class ConnectServer {
         }
     }
 
+    /**
+     * Метод ля проверки связи с сервером по указанным параметрам
+     */
     private void checkingAvailabilityServer(){
 
     }
 
+    /**
+     * Метод возвращиет булево значение о доступности сервера
+     * true - доступен, false -нет.
+     * @return
+     */
     public boolean results(){
         return result;
     }
