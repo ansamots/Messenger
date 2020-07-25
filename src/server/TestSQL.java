073043b15a5016cfd8f9402183732f973f565e2c
ansamots <33481586+ansamots@users.noreply.github.com>
2020-07-25 10:12:14 +0300

    Готова полностью новая база, перенастройка проэкта на эту базу.

--- src/server/TestSQL.java
+++ src/server/TestSQL.java
@@ -5,8 +5,15 @@ import java.sql.SQLException;
 public class TestSQL {
     public static void main (String[] args){
         SQLConnector sqlConnector = new SQLConnector();
-        String admin = "admin";
+        String admin = "admin1";
         String pass = "12345";
+        try{
+            boolean b = sqlConnector.autUser(admin, pass);
+        }catch (SQLException e){
+            System.out.println("Error");
+            System.out.println(e);
+        }
+
         boolean b = sqlConnector.autUser2(admin, pass);
     }
 }
