243eecdde30d52fdd49ef435e02eae4613b79123
ansamots <33481586+ansamots@users.noreply.github.com>
2020-07-29 09:54:42 +0300

    Проверка логина и пароля успешна.

--- src/server/SQLConnector.java
+++ src/server/SQLConnector.java
@@ -4,7 +4,6 @@ package server;
 import java.sql.*;
 
 public class SQLConnector {
-//    public static void main (String[] args){
     SQLConnector(){
         try{
             url = "jdbc:mysql://DESKTOP-N1MNM2T:3306/test_db"; // Путь к базе данных
@@ -16,52 +15,43 @@ public class SQLConnector {
 //            statement.executeUpdate("DROP TABLE table_user"); // Удаляем таблицу sql запросом
 //            statement.executeUpdate("CREATE TABLE table_user"); // Создаём таблицу запросом
 //            statement.executeUpdate("INSERT INTO table_user VALUES (NULL, 'First_User', 'test@email.ru');"); // Вставляем значение в таблицу.
-            select = statement.executeQuery("SELECT * FROM table_user WHERE id = 1;"); // Создаём запрос в базу данных
-            while(select.next()){
-               System.out.println(select.getString(1));
-                System.out.println(select.getString(2));
-                System.out.println(select.getString(3));
-            }
-            System.out.println("All good!");
         }catch(Exception e){
+            System.out.println("Соединение с базой данных не удалось");
             System.out.println(e);
         }
     }
 
-    public boolean autUser(String login, String password) throws SQLException {
-        boolean result = false;
-        String qwery = ("\"SELECT * FROM table_user WHERE mame = '"+login+"';\"");
-        System.out.println(qwery);
-//        select = statement.executeQuery("SELECT * FROM table_user WHERE mame = 'admin';"); // Не работает
-        select = statement.executeQuery("SELECT * FROM table_user;");
-//        select = statement.executeQuery(qwery);
-        while(select.next()){
-            System.out.println(select.getString(1));
-            System.out.println(select.getString(2));
-            System.out.println(select.getString(3));
-        }
-        return result;
-    }
 
-    public boolean autUser2(String login, String password){
-        String qwery2 = ("SELECT * FROM table_user WHERE name = ?;"); // Сборная строка, где за место знака вопроса подставляется значение
+    /**
+     * В таблице которую используем в этом методе следующие индексы
+     *  1 - Первичный ключ записи в таблице
+     *  2 - Логин
+     *  3 - Почта
+     *  4 - Пароль
+     *  5 - Дата последнего выхода из системы
+     */
+    public boolean autUser(String login, String password){ // В этом методе реализован запрос к базе на соответствие логина и пароля пользователя
+        boolean loginResult = false;
+        String qwery = ("SELECT * FROM table_user WHERE name = ?;"); // Сборная строка, где за место знака вопроса подставляется значение
         PreparedStatement preparedStatement = null; // При помощи этого класса будем собирать строку в запрос
         try {
-            preparedStatement = connection.prepareStatement(qwery2); // Передаём строку
+            preparedStatement = connection.prepareStatement(qwery); // Передаём строку
             preparedStatement.setString(1, login); // По индексу знака вопроса вставляем значение
-            System.out.println(preparedStatement);
             select = preparedStatement.executeQuery(); // Выполняем наш сборный запрос
-            while(select.next()){ // Выводим все данные из запроса.
-                System.out.println(select.getString(1));
-                System.out.println(select.getString(2));
-                System.out.println(select.getString(3));
+            while(select.next()){
+                if (login.equals(select.getString(2))){ // Сравниваем полученый логин с логином из базы
+                    if (password.equals(select.getString(4))){ // Сравниваем полученый пароль с паролем из базы
+                        loginResult = true;
+                    }
+                }
             }
-//            preparedStatement.execute();
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        return false;
+        return loginResult;
     }
+
+
     private String url;
     private String login;
     private String password;
--- src/server/TestSQL.java
+++ src/server/TestSQL.java
@@ -1,19 +1,12 @@
 package server;
 
-import java.sql.SQLException;
 
 public class TestSQL {
     public static void main (String[] args){
         SQLConnector sqlConnector = new SQLConnector();
         String admin = "admin1";
-        String pass = "12345";
-        try{
-            boolean b = sqlConnector.autUser(admin, pass);
-        }catch (SQLException e){
-            System.out.println("Error");
-            System.out.println(e);
-        }
-
-        boolean b = sqlConnector.autUser2(admin, pass);
+        String pass = "123451";
+        boolean b = sqlConnector.autUser(admin, pass);
+        System.out.println("Ответ SQL: "+ b);
     }
 }
