ec864db26d069ee7ba0aae0cd9797e82804ce268
ansamots <33481586+ansamots@users.noreply.github.com>
2021-03-13 00:59:05 +0300

    Сохранение скрипта по созданию базы.

---
+++ src/MessengerWeb/Patterns/TestJava.java
@@ -0,0 +1,13 @@
+package MessengerWeb.Patterns;
+
+public class TestJava {
+    public static void main(String[] args){
+        String s = "3[xyz]4[xy]z";
+        char[] sSplit = s.toCharArray();
+        for(int i = 0; i < sSplit.length; i++){
+            if(sSplit[i] == '['){
+
+            }
+        }
+    }
+}
